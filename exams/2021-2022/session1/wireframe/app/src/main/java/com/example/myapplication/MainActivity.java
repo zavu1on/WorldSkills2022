5413e6e4f761c2275454e4a6eab7a1054b7ccb1f
Mikhail <joinmma@gmail.com>
2022-06-20 19:24:51 +0200

    первая сессия

--- exams/2021-2022/session1/wireframe/app/src/main/java/com/example/myapplication/MainActivity.java
+++ exams/2021-2022/session1/wireframe/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.myapplication;
-
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +10,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void guestBtnClickHandler(View view) {
+        Intent intent = new Intent(this, NewsActivity.class);
+        startActivity(intent);
+    }
+
+    public void abonentBtnClickHandler(View view) {
+
     }
 }
